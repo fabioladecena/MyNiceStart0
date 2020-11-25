f01cc3349d2e9debfdf0ff3b8591d8f519e15c8b
fabioladecena <fabidecena@gmail.com>
2020-11-25 15:56:00 +0100

    1er Commit

---
+++ app/src/main/java/com/example/mynicestart/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mynicestart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView txtPrueba = (TextView) findViewById(R.id.textView3);
+        txtPrueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mynicestart/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.example.mynicestart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        openApp(true);
+
+        TextView mySubtitle = (TextView)findViewById(R.id.textView4);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        mySubtitle.startAnimation(myanim);
+        openApp(true);
+    }
+
+    private void openApp(boolean locationPermission) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen
+                        .this, ActivityLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
