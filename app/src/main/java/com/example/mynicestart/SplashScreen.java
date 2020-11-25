283eb9ff34fa5b8ba6433360af47f739930448ed
fabioladecena <fabidecena@gmail.com>
2020-11-25 16:17:51 +0100

    .

--- app/src/main/java/com/example/mynicestart/SplashScreen.java
+++ app/src/main/java/com/example/mynicestart/SplashScreen.java
@@ -33,6 +33,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 5000);
+        }, 6000);
     }
 }
